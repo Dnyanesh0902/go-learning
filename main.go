2f99c23c7ce9336fea37c6bcdffd21cbb2a4ab0e
Dnyaneshwar KOkate <dnyaneshwarkokate@Dnyaneshwars-MacBook-Air.local>
2026-03-10 21:03:19 +0530

    Day 1: Go setup and Hello World program

--- main.go
+++ main.go
@@ -4,4 +4,7 @@ import "fmt"
 
 func main(){
 	fmt.Println("Hello Dnyanesh! My Go Journey Starts Today 🚀.")
+	fmt.Println("Name :Dnyanesh Kokate")
+	fmt.Println("Learning: Go Language")
+	fmt.Println("Goal: Backend Developer")
 }

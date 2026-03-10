d5fd7a152d14a548fd2957f86b7f62527adaeb6e
Dnyaneshwar KOkate <dnyaneshwarkokate@Dnyaneshwars-MacBook-Air.local>
2026-03-10 21:22:07 +0530

    Day 2: Variables and Data Types in Go

---
+++ day2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+func main() {
+	name := "Dnyanesh Kokate"
+	age := 25
+	course := "Go Language"
+	goal := "Backend Developer"
+	
+	fmt.Println("Hello Dnyanesh! My Go Journey Starts Today 🚀.")
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Learning:", course)
+	fmt.Println("Goal:", goal)
+}
